b742ddbaa01a0ee1f81e51abb9f17b41634f86fd
Phuc Tran <phuc.d.tran.24@dartmouth.edu>
2025-05-31 00:18:00 -0400

    Update configuration for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // Needed for GitHub Pages to work with the repository name as the base path
-  basePath: process.env.NODE_ENV === 'production' ? '/matcha-powder' : '',
-  // For GitHub Pages trailing slash is needed
-  trailingSlash: true,
   // Image configuration
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
